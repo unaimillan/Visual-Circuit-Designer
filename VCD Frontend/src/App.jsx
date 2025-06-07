4d55b0210784edb4a35999420cb2bab42877ea3c
rii <albert.khechoyan16@gmail.com>
2025-06-07 14:34:36 +0300

    First prototype's prototype of frontend

---
+++ VCD Frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [panelState, setPanelState] = useState(false)
+
+
+
+  return (
+    <div>
+      <button
+          className="panelButton"
+          onClick={() => setPanelState(!panelState)}
+      >
+          {panelState ? "Закрыть" : "Открыть"} меню.
+      </button>
+
+      <div className={`panel ${panelState ? 'open' : ''}`}>
+
+        <p className={"menuText menuContent"}>
+          Приветик!💋 Я крутая менюшка!
+        </p>
+
+        <button className={`panelInnerButton menuContent ${panelState ? 'showed' : ''}`}>
+
+          Я перви кнопишка! 👻
+        </button>
+
+        <button className={`panelInnerButton menuContent ${panelState ? 'showed' : ''}`}>
+          А я вторая кнопишка! ☺️
+        </button>
+
+        <button className={`panelInnerButton menuContent ${panelState ? 'showed' : ''}`}>
+          Окак! Я трети кнопишка! 💕🎶
+        </button>
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default App
