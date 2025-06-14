661ca3f80a3698f6f1f5d48c01ebd32c3dfcd7ae
kostya2505 <kostya250506@gmail.com>
2025-06-14 23:38:43 +0300

    Handlers position fixed for real

--- VCD Frontend/src/components/basicLogicElements/NOT.jsx
+++ VCD Frontend/src/components/basicLogicElements/NOT.jsx
@@ -24,15 +24,17 @@ function NotNode({ isConnectable }) {
       {/* Handles */}
       <CustomHandle
         type="target"
+        position={Position.Left}
         id="input-1"
-        style={{ top: 34, left: 0 }}
+        style={{ top: 34, left: -1 }}
         isConnectable={isConnectable}
         maxConnections={1}
       />
       <Handle
         type="source"
+        position={Position.Right}
         id="output-1"
-        style={{ top: 34, left: 70 }}
+        style={{ top: 34, left: 72 }}
         isConnectable={isConnectable}
       />
     </div>
