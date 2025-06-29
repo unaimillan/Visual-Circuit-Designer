50928c9683a405a53b3f6fa7fbd98dcb73fc3a90
doshq <matvey4ikk@gmail.com>
2025-06-29 04:28:08 +0300

    Added Runner Node 4 unit tests. Handling lack of required data.

--- RunnerNode/connect.py
+++ RunnerNode/connect.py
@@ -51,7 +51,7 @@ async def internal_simulation_error(sid, data):
 
 
 @sio.on("run_simulation")
-async def run_simulation(sid, circuit_data):
+async def run_simulation(sid, circuit_data=None):
     if sid in user_simulations:
         await sio.emit("error", {"msg": "Simulation already running"}, room=sid)
         return
@@ -62,8 +62,11 @@ async def run_simulation(sid, circuit_data):
 
     try:
         verilog_code = generate_verilog_from_json(circuit_data)
+    except TypeError:
+        await sio.emit("error", {"msg": f"Invalid type of circuit data. Expected 'dict', got {type(circuit_data)}"}, room=sid)
+        return
     except Exception as e:
-        await sio.emit("error", {"msg": str(e)}, room=sim_id)
+        await sio.emit("error", {"msg": str(e)}, room=sid)
         return
 
     verilog_path = os.path.join(sim_path, "dut.v")
--- RunnerNode/unit_tests/test_correct_circuit.py
+++ RunnerNode/unit_tests/test_correct_circuit.py
@@ -4,11 +4,20 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_run_simulation_emits_no_error(socketio_client):
+    result = {}
+    done = asyncio.Event()
     received_errors = []
 
     @socketio_client.on("error")
     def on_error(data):
         received_errors.append(data)
+
+    @socketio_client.on("simulation_outputs")
+    def on_outputs(data):
+        nonlocal result
+        print("Received outputs:", data)
+        result = data
+        done.set()
 
     mock_circuit = {
         "nodes": [
@@ -74,7 +83,16 @@ async def test_run_simulation_emits_no_error(socketio_client):
     }
 
     await socketio_client.emit("run_simulation", mock_circuit)
-
     await asyncio.sleep(1)
 
+    await socketio_client.emit("set_inputs", { "inputs": {"in_inputNodeSwitch1": 1, "in_inputNodeSwitch2": 1}})
+    await asyncio.sleep(1)
+
+    await asyncio.wait_for(done.wait(), timeout=2)
+    await socketio_client.emit("stop_simulation")
+
+    assert result.get("out_outputNodeLed") == 0
+
     assert not received_errors, f"Received errors: {received_errors}"
+
+
