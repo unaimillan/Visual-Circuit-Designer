a433936d0bb31499c1c27f1e670cb0b0354b40a5
arsenez <arsenez@cybercommunity.space>
2025-06-30 17:43:34 +0300

    Fix poco postgre module in nix

--- auth/default.nix
+++ auth/default.nix
@@ -1,11 +1,21 @@
 with import <nixpkgs> {};
 
+let
+  # Переопределяем Poco с поддержкой PostgreSQL
+  pocoWithPostgres = pkgs.poco.overrideAttrs (oldAttrs: {
+    buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.postgresql ];
+    cmakeFlags = (oldAttrs.cmakeFlags or []) ++ [
+      (pkgs.lib.cmakeBool "POCO_ENABLE_DATA_POSTGRESQL" true)
+    ];
+  });
+in
+
 stdenv.mkDerivation {
     name = "VCDAuth";
     src = ./.;
 
     nativeBuildInputs = [ cmake ninja ];
-    buildInputs = [ poco ];
+    buildInputs = [ pocoWithPostgres postgresql ];
 
     configurePhase = "cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release";
     buildPhase = "cmake --build build --config Release";
