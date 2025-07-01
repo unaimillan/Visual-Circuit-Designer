6f3fb2a1e06c2ba733970313101f5327e90365fd
kostya2505 <kostya250506@gmail.com>
2025-07-02 01:06:56 +0300

    Logic gates code optimised

--- UI/src/components/circuits/basicLogicElements/NOR.jsx
+++ UI/src/components/circuits/basicLogicElements/NOR.jsx
@@ -1,70 +1,16 @@
-import { Position } from "@xyflow/react";
-import CustomHandle from "../../codeComponents/CustomHandle.jsx";
+import LogicGateBase from "../../codeComponents/LogicGateBase.jsx";
 import { IconNOR } from "../../../../assets/circuits-icons.jsx";
-import { useRotatedNode } from "../../hooks/useRotatedNode.jsx";
+import { GATE_HANDLE_CONFIGS } from "../../codeComponents/handleConfigs.js";
 
 function NorNode({ id, data, isConnectable }) {
-  const rotation = data.rotation || 0;
-  const { getHandlePosition, RotatedNodeWrapper } = useRotatedNode(id, rotation, 80, 70);
-
-  const getHandleStyle = (handle) => {
-    switch (rotation) {
-      case 90:
-        return handle === "input-1"
-          ? { top: 24, left: -1 }
-          : handle === "input-2"
-            ? { top: 44, left: -1 }
-            : { top: 26.5, left: 79 };
-      case 180:
-        return handle === "input-1"
-          ? { top: 24, left: -8 }
-          : handle === "input-2"
-            ? { top: 44, left: -8 }
-            : { top: 34, left: 79 };
-      case 270:
-        return handle === "input-1"
-          ? { top: 17, left: -1 }
-          : handle === "input-2"
-            ? { top: 37, left: -1 }
-            : { top: 34.5, left: 79 };
-      default:
-        return handle === "input-1"
-          ? { top: 25, left: -1 }
-          : handle === "input-2"
-            ? { top: 45, left: -1 }
-            : { top: 35, left: 72 };
-    }
-  };
-
   return (
-    <RotatedNodeWrapper className="circuit-button">
-      <IconNOR SVGClassName={"circuit-button-icon"} />
-
-      {/* Handles */}
-      <CustomHandle
-        type="target"
-        position={getHandlePosition(Position.Left)}
-        id="input-1"
-        style={getHandleStyle("input-1")}
-        isConnectable={isConnectable}
-        maxConnections={1}
-      />
-      <CustomHandle
-        type="target"
-        position={getHandlePosition(Position.Left)}
-        id="input-2"
-        style={getHandleStyle("input-2")}
-        isConnectable={isConnectable}
-        maxConnections={1}
-      />
-      <CustomHandle
-        type="source"
-        position={getHandlePosition(Position.Right)}
-        id="output-1"
-        style={getHandleStyle("output-1")}
-        isConnectable={isConnectable}
-      />
-    </RotatedNodeWrapper>
+    <LogicGateBase
+      id={id}
+      data={data}
+      isConnectable={isConnectable}
+      IconComponent={IconNOR}
+      handleConfigs={GATE_HANDLE_CONFIGS.NOR}
+    />
   );
 }
 
--- UI/src/components/circuits/basicLogicElements/NOT.jsx
+++ UI/src/components/circuits/basicLogicElements/NOT.jsx
@@ -1,54 +1,17 @@
-import { Position } from "@xyflow/react";
-import CustomHandle from "../../codeComponents/CustomHandle.jsx";
+import LogicGateBase from "../../codeComponents/LogicGateBase.jsx";
 import { IconNOT } from "../../../../assets/circuits-icons.jsx";
-import { useRotatedNode } from "../../hooks/useRotatedNode.jsx";
+import { GATE_HANDLE_CONFIGS, getNotGateHandleStyle } from "../../codeComponents/handleConfigs.js";
 
 function NotNode({ id, data, isConnectable }) {
-  const rotation = data.rotation || 0;
-  const { getHandlePosition, RotatedNodeWrapper } = useRotatedNode(id, rotation, 80, 70);
-
-  const getHandleStyle = (handle) => {
-    switch (rotation) {
-      case 90:
-        return handle === "input-1"
-          ? { top: 34, left: -1 }
-          : { top: 26.5, left: 79 };
-      case 180:
-        return handle === "input-1"
-          ? { top: 34, left: -8 }
-          : { top: 34, left: 79 };
-      case 270:
-        return handle === "input-1"
-          ? { top: 27.5, left: -1 }
-          : { top: 34.5, left: 79 };
-      default:
-        return handle === "input-1"
-          ? { top: 35, left: -1 }
-          : { top: 35, left: 72 };
-    }
-  };
-
   return (
-    <RotatedNodeWrapper className="circuit-button">
-      <IconNOT SVGClassName={"circuit-button-icon"} />
-
-      {/* Handles */}
-      <CustomHandle
-        type="target"
-        position={getHandlePosition(Position.Left)}
-        id="input-1"
-        style={getHandleStyle("input-1")}
-        isConnectable={isConnectable}
-        maxConnections={1}
-      />
-      <CustomHandle
-        type="source"
-        position={getHandlePosition(Position.Right)}
-        id="output-1"
-        style={getHandleStyle("output-1")}
-        isConnectable={isConnectable}
-      />
-    </RotatedNodeWrapper>
+    <LogicGateBase
+      id={id}
+      data={data}
+      isConnectable={isConnectable}
+      IconComponent={IconNOT}
+      handleConfigs={GATE_HANDLE_CONFIGS.NOT}
+      getHandleStyleOverrides={getNotGateHandleStyle}
+    />
   );
 }
 
