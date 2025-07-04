fb42d20ea56671908c32d7a12ab45f6b173e4691
rii <albert.khechoyan16@gmail.com>
2025-07-04 13:51:08 +0300

    Added animations for text in main page

--- UI/src/components/pages/hello-page.jsx
+++ UI/src/components/pages/hello-page.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import StickyBox from "react-sticky-box";
 import "../../CSS/hello-page.css";
-import { VantaFogBackground } from "../../../assets/animated-bg.jsx";
-import { Link } from "react-router-dom";
+import {VantaFogBackground} from "../../../assets/animated-bg.jsx";
+import {Link} from "react-router-dom";
+import {TextEffectTwo} from 'react-text-animate'
 
 import {
   IconBulb,
@@ -40,7 +41,7 @@ function App() {
 
   return (
     <div className={"whole-page"}>
-      <VantaFogBackground />
+      <VantaFogBackground/>
       <div
         className={`backdrop-whole-page ${scrollY > 350 ? "active" : ""}`}
       ></div>
@@ -49,7 +50,7 @@ function App() {
         <div className={"header-left-buttons"}>
           <button className={"header-button"}>Sign Up</button>
 
-          <Link to="/" style={{ textDecoration: "none" }}>
+          <Link to="/" style={{textDecoration: "none"}}>
             <button className={"header-button"}>Get Started</button>
           </Link>
         </div>
@@ -64,12 +65,13 @@ function App() {
         >
           <h1
             className="hello-hero-title"
-            style={{ transform: `translateX(0)`, opacity: "1" }}
+            style={{transform: `translateX(0)`, opacity: "1"}}
           >
-            Design logic circuits in your browser.
+            <TextEffectTwo animationDuration={1} animateOnce={true} text="Design logic circuits in your browser."/>
+
           </h1>
           <p className="hello-hero-subtitle">
-            Create, simulate and sync — instantly.
+            <TextEffectTwo animationDuration={1} animateOnce={true} text="Create, simulate and sync — instantly."/>
           </p>
         </div>
       </div>
@@ -80,10 +82,10 @@ function App() {
             <div className="side-text left">
               <div
                 className={"sticky-upper-block"}
-                style={{ transform: `translateX(${-block1Progress * 30}vw)` }}
+                style={{transform: `translateX(${-block1Progress * 30}vw)`}}
               >
                 <div className={"appear-block-title"}>
-                  <IconThunder SVGClassName={"sticky-icon"} />
+                  <IconThunder SVGClassName={"sticky-icon"}/>
                   Instant Access
                 </div>
                 <p className={"appear-block-description"}>
@@ -94,10 +96,10 @@ function App() {
 
               <div
                 className={"sticky-lower-block"}
-                style={{ transform: `translateX(${-block2Progress * 30}vw)` }}
+                style={{transform: `translateX(${-block2Progress * 30}vw)`}}
               >
                 <div className={"appear-block-title"}>
-                  <IconBulb SVGClassName={"sticky-icon"} />
+                  <IconBulb SVGClassName={"sticky-icon"}/>
                   Simple & Intuitive
                 </div>
                 <p className={"appear-block-description"}>
@@ -118,10 +120,10 @@ function App() {
             <div className="side-text right">
               <div
                 className={"sticky-upper-block"}
-                style={{ transform: `translateX(${block3Progress * 30}vw)` }}
+                style={{transform: `translateX(${block3Progress * 30}vw)`}}
               >
                 <div className={"appear-block-title"}>
-                  <IconSettings SVGClassName={"sticky-icon"} />
+                  <IconSettings SVGClassName={"sticky-icon"}/>
                   Real-Time Simulation
                 </div>
                 <p className={"appear-block-description"}>
@@ -137,7 +139,7 @@ function App() {
                     transform: `translateX(${block4Progress * 30}vw)`,
                   }}
                 >
-                  <IconStar SVGClassName={"sticky-icon"} />
+                  <IconStar SVGClassName={"sticky-icon"}/>
                   Modern & Fast UI
                   <p className={"appear-block-description"}>
                     A sleek, responsive interface built for speed and clarity —
@@ -151,11 +153,12 @@ function App() {
       </div>
 
       <div className={"try-it-wrapper"}>
-        <p style={{ fontSize: "50px", margin: 0, fontWeight: "600" }}>
-          Try it now!
+
+        <p style={{fontSize: "50px", margin: 0, fontWeight: "600"}}>
+          <TextEffectTwo animationDuration={1} animateOnce={true} text="Try it now!"/>
         </p>
 
-        <div style={{ display: "flex", gap: "20px" }}>
+        <div style={{display: "flex", gap: "20px"}}>
           <button className={"try-it-button"}>Go to Library</button>
 
           <Link
