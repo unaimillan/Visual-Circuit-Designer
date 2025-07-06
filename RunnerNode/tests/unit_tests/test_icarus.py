271215ef45a604a0c5c8711678505434bbed8699
doshq <matvey4ikk@gmail.com>
2025-07-06 22:33:22 +0300

    Create Runner icarus verilog unit test

--- RunnerNode/tests/unit_tests/test_icarus.py
+++ RunnerNode/tests/unit_tests/test_icarus.py
@@ -1,23 +1,16 @@
-import asyncio
 import pytest
-from cocotb.runner import get_runner
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=UserWarning)
+    from cocotb.runner import get_runner
 
 
 @pytest.mark.asyncio
-async def test_generator_absence_data():
+async def test_icarus_build():
     runner = get_runner("icarus")
 
     runner.build(
-        verilog_sources=["dut.v"],
+        verilog_sources=["RunnerNode/tests/unit_tests/dut.v"],
         hdl_toplevel="GeneratedCircuit",
         build_dir="build"
     )
-
-    runner.test(
-        hdl_toplevel="GeneratedCircuit",
-        test_module="cocotbTest",
-    )
-
-    await asyncio.sleep(1)
-
-
