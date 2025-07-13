efb224d28bfd0becae5887361257a5f40fd19389
kostya2505 <kostya250506@gmail.com>
2025-07-13 17:36:44 +0300

    Buttons in toolbar now have titles

--- UI/src/components/pages/mainPage/toolbar.jsx
+++ UI/src/components/pages/mainPage/toolbar.jsx
@@ -42,6 +42,7 @@ export default function Toolbar({
         <button
           className={`simulate-button ${simulateState}`}
           onClick={onSimulateClick}
+          title={"Simulation"}
         >
           {simulateState === "idle" && (
             <IconStart
@@ -78,6 +79,7 @@ export default function Toolbar({
             setPanOnDrag([2]);
           }}
           disabled={activeAction === "cursor"}
+          title={"Cursor (Ctrl+1)"}
         >
           <IconToolbarCursor
             SVGClassName="toolbarButtonIcon"
@@ -92,6 +94,7 @@ export default function Toolbar({
             setPanOnDrag(true);
           }}
           disabled={activeAction === "hand"}
+          title={"Hand (Ctrl+2)"}
         >
           <IconToolbarHand SVGClassName="toolbarButtonIcon" draggable="false" />
         </button>
@@ -100,6 +103,7 @@ export default function Toolbar({
           className={`toolbarButton ${activeAction === "eraser" ? "active" : ""}`}
           onClick={() => setActiveAction("eraser")}
           disabled={activeAction === "eraser"}
+          title={"Eraser (Ctrl+3)"}
         >
           <IconToolbarEraser
             SVGClassName="toolbarButtonIcon"
@@ -111,6 +115,7 @@ export default function Toolbar({
           className={`toolbarButton ${activeAction === "text" ? "active" : ""}`}
           onClick={() => setActiveAction("text")}
           disabled={activeAction === "text"}
+          title={"Text (Ctrl+4)"}
         >
           <IconToolbarText SVGClassName="toolbarButtonIcon" draggable="false" />
         </button>
@@ -123,6 +128,7 @@ export default function Toolbar({
             setActiveWire("default");
           }}
           disabled={activeWire === "default"}
+          title={"Default (Ctrl+5)"}
         >
           <IconToolbarBezierWire
             SVGClassName="toolbarButtonIcon"
@@ -136,6 +142,7 @@ export default function Toolbar({
             setActiveWire("step");
           }}
           disabled={activeWire === "step"}
+          title={"Step (Ctrl+6)"}
         >
           <IconToolbarStepWire
             SVGClassName="toolbarButtonIcon"
@@ -149,6 +156,7 @@ export default function Toolbar({
             setActiveWire("straight");
           }}
           disabled={activeWire === "straight"}
+          title={"Straight (Ctrl+7)"}
         >
           <IconToolbarStraightWire
             SVGClassName="toolbarButtonIcon"
@@ -164,7 +172,10 @@ export default function Toolbar({
           disabled={!canUndo}
           title={canUndo ? "Undo (Ctrl+Z)" : "Nothing to undo in this tab"}
         >
-          <IconUndo SVGClassName="toolbarButtonIcon" draggable="false" />
+          <IconUndo
+            SVGClassName="toolbarButtonIcon"
+            draggable="false"
+          />
         </button>
 
         <button
@@ -173,12 +184,19 @@ export default function Toolbar({
           disabled={!canRedo}
           title={canRedo ? "Redo (Ctrl+Y)" : "Nothing to redo in this tab"}
         >
-          <IconRedo SVGClassName="toolbarButtonIcon" draggable="false" />
+          <IconRedo
+            SVGClassName="toolbarButtonIcon"
+            draggable="false"
+          />
         </button>
       </div>
 
       <div className="toolbar download">
-        <button className={`toolbarButton`} onClick={saveCircuit}>
+        <button
+          className={`toolbarButton`}
+          onClick={saveCircuit}
+          title={"Download"}
+        >
           <IconDownloadFile
             SVGClassName="toolbarButtonIcon"
             draggable="false"
@@ -187,11 +205,18 @@ export default function Toolbar({
 
         <div className="toolbar-separator"></div>
 
-        <button className={`toolbarButton `} onClick={handleOpenClick}>
-          <IconOpenFile SVGClassName="toolbarButtonIcon" draggable="false" />
+        <button
+          className={`toolbarButton`}
+          onClick={handleOpenClick}
+          title={"Upload"}
+        >
+          <IconOpenFile
+            SVGClassName="toolbarButtonIcon"
+            draggable="false"
+          />
         </button>
         <input
-          className={`hidden `}
+          className={`hidden`}
           ref={fileInputRef}
           type="file"
           accept=".json"
