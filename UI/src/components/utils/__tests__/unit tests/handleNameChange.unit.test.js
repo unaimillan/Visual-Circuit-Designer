e34a543633087d2ab5b6763806fea0059a1c47ec
kostya2505 <kostya250506@gmail.com>
2025-07-19 16:01:03 +0300

    unit test fixed

--- UI/src/components/utils/__tests__/unit tests/handleNameChange.unit.test.js
+++ UI/src/components/utils/__tests__/unit tests/handleNameChange.unit.test.js
@@ -1,10 +1,7 @@
 import { handleNameChange } from "../../handleNameChange";
 
-jest.useFakeTimers();
-
 describe("handleNameChange", () => {
   const mockSetNodes = jest.fn();
-  const mockRecordHistory = jest.fn();
 
   const editableNode = { id: "1", name: "Old Name" };
 
@@ -15,13 +12,12 @@ describe("handleNameChange", () => {
 
   beforeEach(() => {
     mockSetNodes.mockClear();
-    mockRecordHistory.mockClear();
   });
 
   it("updates the name of the editable node", () => {
     const event = { target: { value: "New Name" } };
 
-    handleNameChange(event, editableNode, mockSetNodes, mockRecordHistory);
+    handleNameChange(event, editableNode, mockSetNodes);
 
     expect(mockSetNodes).toHaveBeenCalledWith(expect.any(Function));
     const updateFn = mockSetNodes.mock.calls[0][0];
@@ -31,17 +27,13 @@ describe("handleNameChange", () => {
       { id: "1", name: "New Name", type: "inputNodeSwitch" },
       { id: "2", name: "Another Node", type: "inputNodeButton" },
     ]);
-
-    jest.runAllTimers();
-    expect(mockRecordHistory).toHaveBeenCalled();
   });
 
   it("does nothing if editableNode is null", () => {
     const event = { target: { value: "Should Not Update" } };
 
-    handleNameChange(event, null, mockSetNodes, mockRecordHistory);
+    handleNameChange(event, null, mockSetNodes);
 
     expect(mockSetNodes).not.toHaveBeenCalled();
-    expect(mockRecordHistory).not.toHaveBeenCalled();
   });
 });
