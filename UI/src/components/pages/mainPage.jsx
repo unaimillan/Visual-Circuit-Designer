10de068f35d3c14b1f266b0afeeb85e62335e28b
kostya2505 <kostya250506@gmail.com>
2025-07-19 14:50:26 +0300

    nodes spawn fixed

--- UI/src/components/pages/mainPage.jsx
+++ UI/src/components/pages/mainPage.jsx
@@ -340,7 +340,6 @@ export default function Main() {
     const { nodes: newNodes, edges: newEdges } = deselectAllUtil(nodes, edges);
     setNodes(newNodes);
     setEdges(newEdges);
-    recordHistory();
   }, [nodes, edges, setNodes, setEdges]);
 
   const deleteSelectedElements = useCallback(() => {
